429f69b25b3bb59ca3d5ee7b882d42069e3e14b7
Alexey <alexey.svekolnikov@gmail.com>
2021-12-06 02:00:51 +0300

    added: View, ViewModel, Properties

---
+++ DesignPatternsWpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using DesignPatternsWpf.Commands;
+
+namespace DesignPatternsWpf.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _title;
+        private string _inputText;
+        private int _counter;
+
+        public MainWindowViewModel()
+        {
+            Title = "Lesson_3";
+
+            ClickCommand = new RelayCommand(ClickExecute, _ => CanClick);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                NotifyPropertyChanged(nameof(Title));
+            }
+        }
+        public string InputText
+        {
+            get => _inputText;
+            set
+            {
+                _inputText = value;
+                NotifyPropertyChanged(nameof(InputText));
+            }
+        }
+        public int Counter
+        {
+            get => _counter;
+            set
+            {
+                _counter = value;
+                NotifyPropertyChanged(nameof(Counter));
+            }
+        }
+
+        public ICommand ClickCommand { get; set; }
+        public bool CanClick => InputText == "start";
+        private void ClickExecute(object obj)
+        {
+            Counter++;
+        }
+    }
+}
---
+++ DesignPatternsWpf/ViewModel/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DesignPatternsWpf.ViewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
